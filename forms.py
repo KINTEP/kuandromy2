b66adaa3e756b2f29e2216fb8c7f41949ffbf296
Sir Isaac Newton <kuminewton@gmail.com>
2019-12-25 16:10:35 +0000

    Changes made to files

--- forms.py
+++ forms.py
@@ -1,19 +1,1 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
 
-class RegistrationForm(FlaskForm):
-	username = StringField("Username", validators = [DataRequired(), Length(min = 2, max = 30)])
-	fullname = StringField("Full Name", validators = [DataRequired()])
-	email = StringField("Email", validators = [DataRequired(), Email()])
-	describe_company = StringField("Describe your company", validators = [DataRequired(), Length(max = 100)])
-	describe_product = StringField("Describe your products", validators = [DataRequired(), Length(max = 100)])
-	password = PasswordField("Password", validators = [DataRequired()])
-	confirm_password = PasswordField("Confirm Password", validators = [DataRequired(), EqualTo("password")])
-	submit = SubmitField("Join Now")
-
-class LoginForm(FlaskForm):
-	fullname = StringField("Full Name", validators = [DataRequired()])
-	email = StringField("Email", validators = [DataRequired(), Email()])
-	password = PasswordField("Password", validators = [DataRequired()])
-	submit = SubmitField("Log In")
